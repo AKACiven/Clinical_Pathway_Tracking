626abbab7e6bd5b2c223d5c21ab4bec77e6c7246
AKACiven <86039868+AKACiven@users.noreply.github.com>
2021-10-03 22:05:00 +0800

    20211003.0

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,8 +5,8 @@ import { getToken } from '@/utils/auth'
 
 // create an axios instance
 const service = axios.create({
-  baseURL: 'http://4py2535603.qicp.vip/',
-  // baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
+  // baseURL: 'http://4py2535603.qicp.vip/',
+  baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
   withCredentials: true, // send cookies when cross-domain requests
   timeout: 5000 // request timeout
 })
